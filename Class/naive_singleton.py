cc1c3d214a3cf94ae98f6f5b4d8e2093b5c5944d
Praful <51020328+praful-potphode@users.noreply.github.com>
2019-11-28 21:52:11 +0530

    Add files via upload

---
+++ Class/naive_singleton.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+from typing import Optional
+
+"""Naïve Singleton
+It’s pretty easy to implement a sloppy Singleton. You just need to hide constructor and implement a static creation method.
+The same class behaves incorrectly in a multithreaded environment. Multiple threads can call the creation method simultaneously
+ and get several instances of Singleton class."""
+
+class SingletonMeta(type):
+    """
+    The Singleton class can be implemented in different ways in Python. Some
+    possible methods include: base class, decorator, metaclass. We will use the
+    metaclass because it is best suited for this purpose.
+    """
+
+    _instance: Optional[Singleton] = None
+
+    def __call__(self) -> Singleton:
+        if self._instance is None:
+            self._instance = super().__call__()
+        return self._instance
+
+
+class Singleton(metaclass=SingletonMeta):
+    def some_business_logic(self):
+        """
+        Finally, any singleton should define some business logic, which can be
+        executed on its instance.
+        """
+
+        # ...
+
+
+if __name__ == "__main__":
+    # The client code.
+
+    s1 = Singleton()
+    s2 = Singleton()
+
+    if id(s1) == id(s2):
+        print("Singleton works, both variables contain the same instance.")
+    else:
+        print("Singleton failed, variables contain different instances.")
+#  Output.txt: Execution result
+# Singleton works, both variables contain the same instance.
